bbbc6f837786205585284a4c502862a728bb9e09
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-02-24 22:57:40 +0200

    set up client scripts
    client script for dom manipulation
    net-utils for asking gameManager for game state and actions

---
+++ frontend/client.js
@@ -0,0 +1,20 @@
+"use strict"
+document.addEventListener("DOMContentLoaded", onLoad);
+
+function onLoad() {
+
+    
+
+
+    function startGame() {//request for creation of a new game
+        
+    }
+    
+    function updateGameState() {//runs every x seconds and asks for data relevant to player   
+        
+    }
+
+    function executeMove() {//looks at move player is trying to make and asks gameManager to perform
+
+    }
+}
---
+++ frontend/net-utils.js
@@ -0,0 +1,12 @@
+"use strict"
+const netUtils = { 
+    login() {
+
+    },
+    getState() {
+
+    },
+    play() {
+        
+    }
+}
