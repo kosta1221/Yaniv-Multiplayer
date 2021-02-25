83503c82cc88c6adfedb43b37c5e72c526f090ac
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-02-25 14:24:03 +0200

    start work on card selection logic

--- frontend/client.js
+++ frontend/client.js
@@ -9,19 +9,18 @@ function onLoad() {
   let matchNumber;
   let pileDeck;
   let activePlayerMove = {
-    "selected-cards": [{}],
+    "selected-cards": [],
   };
   const playerElement = document.querySelector(".active-player");
 
-
   setInterval(updateGameState, 5000);
-  renderAll();
+  // updateGameState();
+  // renderAll();
 
-  
   function startGame() {
     //request for creation of a new game
   }
-  
+
   function updateGameState() {
     //runs every x seconds and asks for data relevant to player
     const state = netUtils.getGameStateForPlayer(myName);
@@ -29,75 +28,109 @@ function onLoad() {
       state.cards,
       state.playersPoints[myName],
       state.playersCardNumbers[myName]
-      );
-      for (const playerName of state.playerNames) {
-        if (playerName === myName) continue;
-        const points = state.playersPoints[playerName];
-        const numberOfCards = state.playersCardNumbers[playerName];
-        opponents[playerName] = new Player(null, points, numberOfCards);
-      }
+    );
+    for (const playerName of state.playerNames) {
+      if (playerName === myName) continue;
+      const points = state.playersPoints[playerName];
+      const numberOfCards = state.playersCardNumbers[playerName];
+      opponents[playerName] = new Player(null, points, numberOfCards);
     }
-
-    function renderAll() {
-      //RENDER PLAYER
-      for (const card of player.cards) {
-        const cardElement = document.createElement("img");
-        const src = `./images/cards-svg/${getImageName(card)}.svg`;
-        cardElement.setAttribute("src", src);
-        cardElement.setAttribute("suit", `${card.suit}`);
-        cardElement.setAttribute("rank", `${card.rank}`);
-        cardElement.setAttribute("is-joker", `${card.isJoker}`);
-        cardElement.classList.add("card");
-        playerElement.appendChild(cardElement);
-      }
-      //RENDER OPPONENTS
-      const opponentsElements = document.querySelectorAll(".opponent");
-      let j = 0;
-      for (const opponentName in opponents) {
-        const opponentElement = opponentsElements.item(j);
-        j++;
-        const opponent = opponents[opponentName];
-        for(let i = 0; i < opponent.numberOfCards; i++) {
-          const cardElement = document.createElement("img");
-          const src = `./images/cards-svg/Card_back.svg`;
-          cardElement.setAttribute("src", src);
-          cardElement.classList.add("card");
-          opponentElement.appendChild(cardElement);
-        }
-      }
-      //RENDER PILE
-
-    }
-
-  function collectMoveData(e) {
-    const cardName = card.getAttribute("suit")
-    const cardSuit = card.getAttribute("rank")
-    const cardSuit = card.getAttribute("rank")
-    activePlayerMove["selected-cards"].push({name: cardName, suit: cardSuit})
   }
 
-  playerElement.addEventListener("click",(e) => {
-    let clickedCard = e.target
-    collectMoveData(clickedCard)
-    verifyMoveEligibility(clickedCard)
-  })
+  function renderAll() {
+    //RENDER PLAYER
+    for (const card of player.cards) {
+      const cardElement = document.createElement("img");
+      const src = `./images/cards-svg/${getImageName(card)}.svg`;
+      cardElement.setAttribute("src", src);
+      cardElement.setAttribute("suit", `${card.suit}`);
+      cardElement.setAttribute("rank", `${card.rank}`);
+      cardElement.setAttribute("is-joker", `${card.isJoker}`);
+      cardElement.classList.add("card");
+      playerElement.appendChild(cardElement);
+    }
+    //RENDER OPPONENTS
+    const opponentsElements = document.querySelectorAll(".opponent");
+    let j = 0;
+    for (const opponentName in opponents) {
+      const opponentElement = opponentsElements.item(j);
+      j++;
+      const opponent = opponents[opponentName];
+      for (let i = 0; i < opponent.numberOfCards; i++) {
+        const cardElement = document.createElement("img");
+        const src = `./images/cards-svg/Card_back.svg`;
+        cardElement.setAttribute("src", src);
+        cardElement.classList.add("card");
+        opponentElement.appendChild(cardElement);
+      }
+    }
+    //RENDER PILE
+  }
 
-  function verifyMoveEligibility(card) {//takes a card from player hand and checks if is selectable by comparison to selected cards
-    const state = netUtils.getGameStateForPlayer(myName);
-    const currentPileTop = state.openCard[openCard.length-1]
-    const selectedCard = activePlayerMove["selected-cards"] //object array of active cards [{}]
-    if(selectedCard[0].name && selectedCard[0].suit === currentPileTop[0].name && currentPileTop[0].suit) {
-      
+  function collectMoveData(card) {
+    //block unplayable cards
+    const cardRank = card.getAttribute("rank");
+    const cardSuit = card.getAttribute("suit");
+    const is_Joker = card.getAttribute("is-joker");
+    const unavailableCards = [];
+    activePlayerMove["selected-cards"].push({
+      rank: cardRank,
+      suit: cardSuit,
+      isJoker: is_Joker,
+    });
+    player.activeCards = activePlayerMove["selected-cards"];
+    for (const card of player.cards) {
+      if (card.suit !== player.activeCards[0].suit) {
+        unavailableCards.push(card);
+      }
+    }
+    console.log(unavailableCards);
+    // for (let i = 2; i < unavailableCards.length; i++) {
+    //   const unavailableCard = unavailableCards[i];
+    //   document.querySelector(
+    //     `body > div > div.box.player.active-player > img:nth-child(${i})`
+    //   );
+    // }
+    // if (!player.activeCards) {
+
+    // }
+    // else {
+    //   console.log(player);
+    //   for (const card of player.cards) {
+    //     if (
+    //       card.suit !== player.activeCards[0].suit &&
+    //       card.name !== player.activeCards[0].name
+    //     ) {
+    //       console.log(card.suit);
+    //       console.log(player.activeCards[0].suit);
+    //     }
+    //   }
+    // }
+  }
+
+  playerElement.addEventListener("click", (e) => {
+    let clickedCard = e.target;
+    collectMoveData(clickedCard);
+    // verifyMoveEligibility(clickedCard);
+  });
+
+  function verifyMoveEligibility(card) {
+    //takes a card from player hand and checks if is selectable by comparison to selected cards
+    const state = netUtils.getState(myName);
+    const currentPileTop = state.openCard[openCard.length - 1];
+    const selectedCard = activePlayerMove["selected-cards"]; //object array of active cards [{}]
+    if (
+      selectedCard[0].name &&
+      selectedCard[0].suit === currentPileTop[0].name &&
+      currentPileTop[0].suit
+    ) {
     }
     if (selectedCard.length > 1) {
-    }
-    else {
-
+    } else {
     }
   }
 
   function executeMove() {
-
     //looks at move player is trying to make and asks gameManager to perform
     /*{
             move: //"place" | "yaniv" | "assaf",
