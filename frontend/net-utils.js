a4c83fad7745afb1e8fa1f4cc14495080a11ba72
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-02-26 22:52:20 +0200

    remove old mock

--- frontend/net-utils.js
+++ frontend/net-utils.js
@@ -1,44 +1,4 @@
-"use strict";
-const mock = {
-  players: ["koren", "kosta", "dvir"],
-  playersReady: {
-    koren: true,
-    kosta: true,
-    dvir: true,
-  },
-  state(name) {
-    const pd = new PileDeck();
-    // pd.createNewFullDeck()
-    pd.cards.push(
-      new Card("spades", "10", false),
-      new Card("clubs", "king", false)
-    );
-    const state = {
-      playersPoints: {
-        koren: 15,
-        kosta: 12,
-        dvir: 21,
-      },
-      playersCardNumbers: {
-        koren: 3,
-        kosta: 1,
-        dvir: 2,
-      },
-      cards: [
-        new Card("hearts", "jack", false),
-        new Card("spades", "10", false),
-        new Card("clubs", "king", false),
-        new Card("diamonds", "2", false),
-        new Card("hearts", "queen", false),
-      ],
-      pileDeck: pd,
-      playerInTurn: name,
-      playerNames: this.players,
-    };
-    state.playersCardNumbers[name] = 5;
-    return state;
-  },
-};
+"use strict"
 
 const netUtils = {
   getWaitingGames() {},
